4c663ce0fe3d031c3f064a70b211e830028c1e6d
Akash Sahani <sahaniaakash563@gmail.com>
2025-02-11 23:53:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+    //Email Sender 
+    const mailer = require('nodemailer')
+
+    let transport=mailer.createTransport({
+        service:'gamil',
+        auth:{
+            user:'sahaniaakash563@gmail.com',
+            pass:'password123@'
+        }
+    })
+
+    let messageOb={
+        from:'sahaniaakash563@gmail.com',
+        to:'2203051050834@paruluniversity.ac.in',
+        subject:'Email Using NODE JS',
+        text:'This mail is sent suing Node js'
+    }
+    transport.sendMail(messageOb,(err,info)=>{
+        if(err){
+            console.log("Error Occured!!"+err);
+        }else{
+            console.log("Email Sent Successfully")
+            console.log(info.res)
+        }
+    })
